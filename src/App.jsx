0df51ee4e755ace5fdf198dcb59ed0644b22ef76
juliemeta <juliemonefeldt_404@hotmail.com>
2026-01-09 09:45:09 +0100

    imported project files from code sandbox to my computer

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,11 @@
-import { useState } from "react";
-import "./App.css";
+import "./styles.css";
 import Weather from "./Weather";
 
-function App() {
-  const [count, setCount] = useState(0);
-
+export default function App() {
   return (
-    <>
-      <h1>Hello World</h1>
-      <Weather city="Odense" />
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-    </>
+    <div className="App">
+      <h1>Weather Search</h1>
+      <Weather />
+    </div>
   );
 }
-
-export default App;
--- src/Weather.jsx
+++ src/Weather.jsx
@@ -1,17 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
 export default function Weather(props) {
-  function handleResponse(response) {
-    alert(
-      `The temperature in ${props.city} is ${Math.round(
-        response.data.temperature.current
-      )}°C`
-    );
-  }
-  let apiKey = "c2t7ea4432f52e0o6d402fd54c5bc269";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${props.city}&key=${apiKey}`;
+  const [city, setCity] = useState("");
+  const [loaded, setLoaded] = useState(false);
+  const [weather, setWeather] = useState({});
 
-  axios.get(apiUrl).then(handleResponse);
-  return <h2>Hello from Weather</h2>;
+  function displayWeather(response) {
+    setLoaded(true);
+    setWeather({
+      city: response.data.city,
+      temperature: response.data.temperature.current,
+      description: response.data.condition.description,
+      wind: response.data.wind.speed,
+      humidity: response.data.temperature.humidity,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "c2t7ea4432f52e0o6d402fd54c5bc269";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(displayWeather);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  let form = (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="search"
+        placeholder="Search for a city"
+        onChange={updateCity}
+      />
+      <button type="submit">Search</button>
+    </form>
+  );
+
+  if (loaded) {
+    return (
+      <div>
+        {form}
+        <h2>{weather.city}</h2>
+        <ul>
+          <li>
+            Temperature is {Math.round(weather.temperature)} degrees celsius
+          </li>
+          <li>Weather is {weather.description}</li>
+          <li>Humidity is {Math.round(weather.humidity)}%</li>
+          <li>Wind is {Math.round(weather.wind)} km/h</li>
+        </ul>
+      </div>
+    );
+  } else {
+    return form;
+  }
 }
