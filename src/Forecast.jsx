4ced2aa68c017e75873a3b218ea5b088cb420196
juliemeta <juliemonefeldt_404@hotmail.com>
2026-01-14 14:03:42 +0100

    Updated Forecast.jsx

--- src/Forecast.jsx
+++ src/Forecast.jsx
@@ -1,33 +1,51 @@
-import React from "react";
+import React, { use, useState } from "react";
 import "./Forecast.css";
 import axios from "axios";
 
 export default function Forecast(props) {
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
+
   function displayForecast(response) {
+    setLoaded(true);
+    setForecast(response.data.daily);
+
     console.log(response.data);
   }
-  let apiKey = "c2t7ea4432f52e0o6d402fd54c5bc269";
-  let city = props.city;
-  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units{metric}`;
 
-  axios.get(apiUrl).then(displayForecast);
-
-  return (
-    <div className="Forecast">
-      <div className="container">
-        <div className="row">
-          <div className="col">
-            <ul>
-              <li className="weekday">Day 1</li>
-              <li>
-                <span className="temp-day">20°</span>{" "}
-                <span className="temp-night">9°</span>
-              </li>
-              <li className="forecast-condition">Cloudy</li>
-            </ul>
+  if (loaded) {
+    return (
+      <div className="Forecast">
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <ul>
+                <li className="weekday">Day 1</li>
+                <li>{forecast[0].condition.icon}</li>
+                <li>
+                  <span className="temp-day">
+                    {Math.round(forecast[0].temperature.maximum)}°
+                  </span>{" "}
+                  <span className="temp-night">
+                    {forecast[0].temperature.minimum}°
+                  </span>
+                </li>
+                <li className="forecast-condition">
+                  {forecast[0].condition.description}
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let apiKey = "c2t7ea4432f52e0o6d402fd54c5bc269";
+    let city = props.city;
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units{metric}`;
+
+    axios.get(apiUrl).then(displayForecast);
+
+    return null;
+  }
 }
